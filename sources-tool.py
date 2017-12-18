15b44c4e88980c38a4cb74ab4bef34bef552efd8
Videonauth <videonauth@googlemail.com>
2017-12-18 13:59:25 +0100

    Implemented portage from default_constants to _constants in local variables. Moved portage from default_settings to _settings to local variables. Implemented portage from default_blueprints to _blueprints in local variables. Updated settings read in from file in code and comment.

--- sources-tool.py
+++ sources-tool.py
@@ -24,6 +24,9 @@ from modules.parser import *
 _time = time.time()
 _path = os.path.dirname(os.path.realpath(__file__))
 _file = 'sources-tool.py'
+_constants = default_constants
+_settings = default_settings
+_blueprints = default_blueprints
 
 if __name__ == '__main__':
     # Create an argparse parser object and uses it to fetch command-line options in a directory.
@@ -38,11 +41,12 @@ if __name__ == '__main__':
         exit(0)
 
     # Check if 'default.conf' exists and if yes read it, if not use default from variables and write file.
-    _settings = default_settings
+    # The situation of a missing default.conf file should not happen leaving the function in for the
+    # rare occurrence of the user deleting the file, then writing a new one from _blueprints.get('default_conf')
     if file_exists(f'{_path}/config/default.conf'):
         _settings.update(file_to_dict(f'{_path}/config/default.conf'))
-    # else:
-    #     dict_to_file(_settings, f'{_path}/config/default.conf')
+    else:
+        list_to_file(_blueprints.get('default_conf'), f'{_path}/config/default.conf')
 
     # Reached the end of the file exiting.
     print(f'Process finished after {time.time() - _time} seconds.')
