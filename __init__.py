823672624b01dc8375ebfd1112ee3186c089f53c
Videonauth <videonauth@googlemail.com>
2017-12-18 17:37:48 +0100

    Travis CI update

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# ---------------------------------------------------------------------------
+# ubuntu-sources-tool - __init__.py
+# ---------------------------------------------------------------------------
+# Author: Videonauth <videonauth@googlemail.com>
+# License: MIT (see LICENSE file)
+# Date: 18.12.17 - 17:33
+# Purpose: -
+# Written for: Python 3.6.3
+# ---------------------------------------------------------------------------
