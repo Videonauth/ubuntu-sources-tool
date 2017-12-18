4eee5e048faa676704518328ffbf3dd0af4d4dcd
Videonauth <Videonauth@users.noreply.github.com>
2017-12-18 14:06:36 +0100

    Changed __version__ to 0.0.5

--- modules/variables.py
+++ modules/variables.py
@@ -11,7 +11,7 @@
 # ---------------------------------------------------------------------------
 
 # Version string
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 # Program defaults
 default_constants = dict(
