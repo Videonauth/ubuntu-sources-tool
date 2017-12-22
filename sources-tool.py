e3f3544c1f2039de7ae16a8ce9303e1198f58ef3
Videonauth <videonauth@googlemail.com>
2017-12-22 18:32:29 +0100

    Style change on import statements.

--- sources-tool.py
+++ sources-tool.py
@@ -17,10 +17,18 @@ import time
 import os
 
 # Local imports
-from modules.variables import default_constants, default_settings
-from modules.blueprints import default_blueprints
-from modules.core import dict_is_all_none, file_to_dict, list_to_file, file_exists
-from modules.parser import parser
+from modules.variables import \
+    default_constants,\
+    default_settings
+from modules.blueprints import \
+    default_blueprints
+from modules.core import \
+    dict_is_all_none,\
+    file_to_dict,\
+    list_to_file,\
+    file_exists
+from modules.parser import \
+    parser
 
 # Local variables
 _time = time.time()
@@ -38,12 +46,12 @@ if __name__ == '__main__':
     # Check command-line options if any are given, if not print usage and exit out.
     if dict_is_all_none(_commands):
         _parser.print_usage()
-        print(f'Process finished after {time.time() - _time} seconds.')
+        print(f'Process finished after { time.time() - _time } seconds.')
         print(f'Process finished with exit code 0.')
         exit(0)
 
     # Check if 'default.conf' exists and if yes read it, if not use default rom variables and write file.
-    # The situation of a missing default.conf ile should not happen leaving the function in for the rare
+    # The situation of a missing default.conf file should not happen leaving the function in for the rare
     # occurrence of the user deleting the file, then writing a new one from _blueprints.get('default_conf')
     if file_exists(f'{_path}/config/default.conf'):
         _settings.update(file_to_dict(f'{_path}/config/default.conf'))
